458c1db72d40ba67653678a58b9b3d8d814c4a43
Abhinand SD <abhinandssd90@gmail.com>
2026-03-15 21:18:17 +0530

    update name social url's

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,11 +46,11 @@ const Navbar = () => {
           RC
         </a>
         <a
-          href="mailto:rajeshchittyal21@gmail.com"
+          href="mailto:abhinandsd49@gmail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          rajeshchittyal21@gmail.com
+          abhinandsd49@gmail.com
         </a>
         <ul>
           <li>
